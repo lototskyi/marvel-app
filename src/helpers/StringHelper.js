a22d866600044e65ddd623c2ae6484f248781487
alex <o.lototskyi@gmail.com>
2022-10-21 17:41:00 +0300

    random character module

---
+++ src/helpers/StringHelper.js
@@ -0,0 +1,8 @@
+class StringHelper {
+
+    truncate = (str, n) => {
+        return (str.length > n) ? str.slice(0, n-1) + '...' : str;
+    }
+}
+
+export default StringHelper;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,41 @@
+import StringHelper from "../helpers/StringHelper";
+
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = `apikey=${process.env.REACT_APP_API_KEY}`;
+
+    stringHelper = new StringHelper();
+
+    getResource = async (url) => {
+        const res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    };
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? this.stringHelper.truncate(char.description, 210) : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
