2ae4fa2e3943857c16749b4a3e7725962ebeb162
alex <o.lototskyi@gmail.com>
2022-10-24 15:43:41 +0300

    set default offest 210

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -13,7 +13,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1543,
+        offset: 210,
         charEnded: false
     }
 
