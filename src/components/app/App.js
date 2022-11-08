aa9e5c1b14a9cf83374a854286fc889d62951474
alex <o.lototskyi@gmail.com>
2022-11-08 19:12:02 +0200

    added router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,52 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import AppHeader from "../appHeader/AppHeader";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-// import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
-// import decoration from '../../resources/img/vision.png';
+import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-    // const [selectedChar, setChar] = useState(null);
+    const [selectedChar, setChar] = useState(null);
 
-    // const onCharSelected = (id) => {
-    //     setChar(id);
-    // }
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <ErrorBoundary>
+                                <RandomChar/>
+                            </ErrorBoundary>
+                            <div className="char__content">
+                                <ErrorBoundary>
+                                    <CharList onCharSelected={onCharSelected}/>
+                                </ErrorBoundary>
+                                <ErrorBoundary>
+                                    <CharInfo charId={selectedChar}/>
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <AppBanner/>
+                            <ComicsList/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 
 }
