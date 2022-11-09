41d1553d10b4440f600dd72be9cfb8c69b8acddf
alex <o.lototskyi@gmail.com>
2022-11-09 11:42:59 +0200

    react-router-dom v5, group all page components by using pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,8 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from "../pages";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
         <Router>
             <div className="app">
@@ -26,29 +10,16 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/">
-                            <ErrorBoundary>
-                                <RandomChar/>
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected}/>
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar}/>
-                                </ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage/>
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner/>
-                            <ComicsList/>
+                            <ComicsPage/>
                         </Route>
                     </Switch>
                 </main>
             </div>
         </Router>
     )
-
 }
 
 export default App;
