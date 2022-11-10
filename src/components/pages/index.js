0162551f2effc58422385ed31ae585a68b808347
alex <o.lototskyi@gmail.com>
2022-11-10 09:55:52 +0200

    lazy components

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,6 +1,5 @@
 import MainPage from "./MainPage";
 import ComicsPage from "./ComicsPage";
 import SingleComicPage from "./SingleComicPage";
-import Page404 from "./404";
 
-export {MainPage, ComicsPage, SingleComicPage, Page404};
+export {MainPage, ComicsPage, SingleComicPage};
